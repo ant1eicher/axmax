0d795b568d9e2937da773fb75e6587684fb81069
Anton Eicher <jediant@gmail.com>
2024-07-30 22:37:15 +0200

    Wekking metrics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use anyhow::Result;
+use aws_config::BehaviorVersion;
+use aws_sdk_cloudwatch::{
+    types::{MetricDatum, StandardUnit},
+    Client,
+};
 use data::{
     qpgs::QPGS,
     qpigs::QPIGS,
@@ -8,8 +14,17 @@ use log::info;
 pub mod data;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     env_logger::init();
+
+    // Load the AWS configuration
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .region("af-south-1")
+        .load()
+        .await;
+
+    // Create the CloudWatch client
+    let client = Client::new(&config);
 
     let result = fetch_command_data_usb(RAW_HID0, "QID")
         .await
@@ -28,19 +43,15 @@ async fn main() {
         .await
         .expect("Failed to read response");
 
-    let qpigs = QPIGS::new_from_string(&result);
-    info!("nverter 1 QPIGS: {:#?}", qpigs);
-
-    println!("");
+    let i1qpigs = QPIGS::new_from_string(&result).expect("Failed to parse");
+    info!("nverter 1 QPIGS: {:#?}", i1qpigs);
 
     let result = fetch_command_data_usb(RAW_HID1, "QPGS1")
         .await
         .expect("Failed to read response");
 
-    let qpgs = QPGS::new_from_string(&result);
-    info!("inverter 1 QPGS: {:#?}", qpgs);
-
-    println!("");
+    let i1qpgs = QPGS::new_from_string(&result).expect("Failed to parse");
+    info!("inverter 1 QPGS: {:#?}", i1qpgs);
 
     let result = fetch_command_data_usb(RAW_HID1, "QID")
         .await
@@ -59,17 +70,76 @@ async fn main() {
         .await
         .expect("Failed to read response");
 
-    let qpigs = QPIGS::new_from_string(&result);
-    info!("inverter 2 QPIGS: {:#?}", qpigs);
-
-    println!("");
+    let i2qpigs = QPIGS::new_from_string(&result).expect("Failed to parse");
+    info!("inverter 2 QPIGS: {:#?}", i2qpigs);
 
     let result = fetch_command_data_usb(RAW_HID1, "QPGS2")
         .await
         .expect("Failed to read response");
 
-    let qpgs = QPGS::new_from_string(&result);
-    info!("inverter 2 QPGS: {:#?}", qpgs);
+    let i2qpgs = QPGS::new_from_string(&result).expect("Failed to parse");
+    info!("inverter 2 QPGS: {:#?}", i2qpgs);
 
-    println!("");
+    // Define the metric data
+    let i1pv1watts = MetricDatum::builder()
+        .metric_name("Inverter1Pv1Watts")
+        .unit(StandardUnit::Count)
+        .value(i1qpigs.pv1_charging_power as f64)
+        .build();
+
+    let i1pv2watts = MetricDatum::builder()
+        .metric_name("Inverter1Pv2Watts")
+        .unit(StandardUnit::Count)
+        .value(i1qpgs.pv2_charging_power as f64)
+        .build();
+
+    let i2pv1watts = MetricDatum::builder()
+        .metric_name("Inverter2Pv1Watts")
+        .unit(StandardUnit::Count)
+        .value(i2qpigs.pv1_charging_power as f64)
+        .build();
+
+    let i2pv2watts = MetricDatum::builder()
+        .metric_name("Inverter2Pv2Watts")
+        .unit(StandardUnit::Count)
+        .value(i2qpgs.pv2_charging_power as f64)
+        .build();
+
+    let battery_percentage = MetricDatum::builder()
+        .metric_name("BatteryPercentage")
+        .unit(StandardUnit::Percent)
+        .value(i1qpigs.battery_capacity_percent as f64)
+        .build();
+
+    let load_percentage = MetricDatum::builder()
+        .metric_name("LoadPercentage")
+        .unit(StandardUnit::Percent)
+        .value(i1qpigs.load_percent as f64)
+        .build();
+
+    let load_watts = MetricDatum::builder()
+        .metric_name("LoadWatts")
+        .unit(StandardUnit::Count)
+        .value((i1qpigs.ac_output_watts + i2qpigs.ac_output_watts) as f64)
+        .build();
+
+    // Send the metric data to CloudWatch
+    let response = client
+        .put_metric_data()
+        .namespace("Home/Solar")
+        .set_metric_data(Some(vec![
+            i1pv1watts,
+            i1pv2watts,
+            i2pv1watts,
+            i2pv2watts,
+            battery_percentage,
+            load_percentage,
+            load_watts,
+        ]))
+        .send()
+        .await?;
+
+    info!("PutMetricData response: {:?}", response);
+
+    Ok(())
 }
